7203052ab3008316ca9db378cebcdf8531bf5b6e
Tijani <73607974+tijanidian@users.noreply.github.com>
2022-04-19 11:52:28 +0200

    WIP TyperConverter

--- app/src/main/java/cripto/potencial/tjdian/app/di/db/AppDataBase.kt
+++ app/src/main/java/cripto/potencial/tjdian/app/di/db/AppDataBase.kt
@@ -2,6 +2,7 @@ package cripto.potencial.tjdian.app.di.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import cripto.potencial.tjdian.features.cryptocurrency.data.local.db.dao.CoinDao
 import cripto.potencial.tjdian.features.cryptocurrency.data.local.db.entities.CoinEntity
 
@@ -12,6 +13,7 @@ import cripto.potencial.tjdian.features.cryptocurrency.data.local.db.entities.Co
     exportSchema = false
 )
 
+@TypeConverters(Converters::class)
 abstract class AppDataBase : RoomDatabase() {
 
     abstract fun coinDao(): CoinDao
--- app/src/main/java/cripto/potencial/tjdian/app/di/db/Converters.kt
+++ app/src/main/java/cripto/potencial/tjdian/app/di/db/Converters.kt
@@ -1,9 +1,35 @@
 package cripto.potencial.tjdian.app.di.db
 
 import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import cripto.potencial.tjdian.features.cryptocurrency.data.local.db.entities.CoinMarketDataLocalModel
+import cripto.potencial.tjdian.features.cryptocurrency.domain.CoinModel
+import cripto.potencial.tjdian.features.cryptocurrency.domain.CurrentPriceModel
 
 @ProvidedTypeConverter
 class Converters {
 
+    @TypeConverter
+    fun jsonToCoinModel(json:String):List<CoinModel> = Gson().fromJson(
+        json,object : TypeToken<List<CoinModel>>(){}.type
+    )
+
+    @TypeConverter
+    fun coinModelToJson(localModel: List<CoinModel>): String = Gson().toJson(
+        localModel, object : TypeToken<List<CoinModel>>() {}.type
+    )
+
+
+    @TypeConverter
+    fun jsonToMarketLocalModel(json:String):List<CoinMarketDataLocalModel> = Gson().fromJson(
+        json,object : TypeToken<List<CoinMarketDataLocalModel>>(){}.type
+    )
+
+    @TypeConverter
+    fun marketLocalModelToJson(localModel: List<CoinMarketDataLocalModel>): String = Gson().toJson(
+        localModel, object : TypeToken<List<CoinMarketDataLocalModel>>() {}.type
+    )
 
 }
