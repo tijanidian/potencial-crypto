ef9913c7fce521f65d5759c9f4c45a6157de65bc
Tijani <73607974+tijanidian@users.noreply.github.com>
2022-04-26 14:15:54 +0200

    WIP  crypto potential

--- app/src/main/java/cripto/potencial/tjdian/features/cryptopotential/presentation/CryptoPotentialFragment.kt
+++ app/src/main/java/cripto/potencial/tjdian/features/cryptopotential/presentation/CryptoPotentialFragment.kt
@@ -1,9 +1,11 @@
 package cripto.potencial.tjdian.features.cryptopotential.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import cripto.potencial.tjdian.databinding.FragmentPotentialCryptoBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,52 +31,34 @@ class CryptoPotentialFragment @Inject constructor() : Fragment() {
 
 
     //Dividimos market cap entre su max supply y colocamos las monedas para calcular
-    private fun calculatePotentialCoin() {
-        binding.labelResult.text = "Rellena todos los campos"
-
-
-        val marketCap = binding.editMarketCap.inputType
-        val totalSupply = binding.editTotalSupplu.inputType
-        val coins = binding.editCoins.inputType
-
-        if (marketCap==null || totalSupply==null|| coins==null) {
-            binding.labelResult.text = "Rellena todos los campos"
-        } else {
-
-            binding.actionCalculatePotential.setOnClickListener {
-                val result=(marketCap/totalSupply)*coins
-                binding.labelResult.text= "${result} $"
-            }
-        }
-
-    }
-
+    @SuppressLint("SetTextI18n")
     private fun calculatePotentialCoin2() {
 
-
-
-            binding.actionCalculatePotential.setOnClickListener {
-                val value1= binding.editMarketCap.text.toString().toDouble()
-                val value2= binding.editTotalSupplu.text.toString().toDouble()
+        binding.actionCalculatePotential.setOnClickListener {
+            if (binding.editMarketCap.text.toString().isEmpty()) {
+                Toast.makeText(requireContext(), "No Puedes dejar campos vacios", Toast.LENGTH_SHORT).show()
+            } else {
+                val value1 = binding.editMarketCap.text.toString().toDouble()
+                val value2 = binding.editTotalSupplu.text.toString().toDouble()
                 val value3 = binding.editCoins.text.toString().toDouble()
-
 
                 val calculatedValue = value1 / value2 * value3
                 val roundoff = (calculatedValue * 100.0).roundToInt() / 100.0
-                binding.labelResult.text = roundoff.toString()
-
-
+                binding.labelResult.text = "$roundoff $"
+            }
+            
         }
 
 
     }
+
 
     private fun resetPotentialCoin() {
         binding.actionResetPotential.setOnClickListener {
             binding.editMarketCap.setText("")
             binding.editTotalSupplu.setText("")
             binding.editCoins.setText("")
-            binding.labelResult.text=""
+            binding.labelResult.text = ""
         }
     }
 
